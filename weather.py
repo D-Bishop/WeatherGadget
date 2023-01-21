6f79224a05d4f410bc48451e8973eb0e676cf97d
D-Bishop <44522687+D-Bishop@users.noreply.github.com>
2023-01-20 19:18:44 -0500

    Get data from weather.gov
    v1 of python code to read & parse data from weather.gov
    - hard-coded for Sewickley
    - retreives JSON

---
+++ weather.py
@@ -0,0 +1,18 @@
+# D Bishop & M Bishop
+# 2023 01 18
+
+# Read data from the NWS & parse
+ 
+import urllib.request 
+import json
+ 
+# read weather from URL to get string & parse JSON to a python dictionary
+response = urllib.request.urlopen('https://api.weather.gov/gridpoints/PBZ/70,69/forecast')  # Sewickley, PA
+weather_dict = json.loads(response.read())
+
+for i in range(4):
+	print(weather_dict['properties']['periods'][i]['name'])
+	print('Temperature ' + str(weather_dict['properties']['periods'][i]['temperature']) + "˚F")
+	print('Wind ' + weather_dict['properties']['periods'][i]['windSpeed'])
+	print(weather_dict['properties']['periods'][i]['shortForecast'])
+	print("")
