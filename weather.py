cf7e4632f26e4c747c82becfce0f257844137e90
LedUmbrella <33603805+LedUmbrella@users.noreply.github.com>
2023-01-20 22:08:00 -0500

    Update weather.py
    removed HEAD

--- weather.py
+++ weather.py
@@ -6,7 +6,6 @@
 import urllib.request 
 import json
 
-<<<<<<< HEAD
 def get_weather():
     # read weather from URL to get string & parse JSON to a python dictionary
     response = urllib.request.urlopen('https://api.weather.gov/gridpoints/PBZ/70,69/forecast')  # Sewickley, PA
@@ -23,11 +22,10 @@ def print_weather(weather_dict):
 
 #wd = get_weather()
 #print_weather(wd)
-=======
+
 for i in range(4):
 	print(weather_dict['properties']['periods'][i]['name'])
 	print('Temperature ' + str(weather_dict['properties']['periods'][i]['temperature']) + "˚F")
 	print('Wind ' + weather_dict['properties']['periods'][i]['windSpeed'] + " from the " + weather_dict['properties']['periods'][i]['windDirection'])
 	print(weather_dict['properties']['periods'][i]['shortForecast'])
 	print("")
->>>>>>> cfdbe457229705c40478a9cea2143bbe8ab979c4
